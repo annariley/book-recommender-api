e8c99d7016fe4d2c8b330d16df23b6d7d3102906
annariley <annamriley4@gmail.com>
2021-05-07 11:33:49 -0700

    testing

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ user_put_args.add_argument("book 5 id", type=int, help="book 5 id required", req
 user_put_args.add_argument("book 5 rating", type=int, help="book 5 rating required", required=True)
 
 def abort_if_id_dne(user_id):
-    if collect.findOne({"_id":user_id}).size()==0:
+    if db.collect.findOne({"_id":user_id}).size()==0:
         abort(404, message="Could not find user")
 
 def abort_if_id_exists(user_id):
-    if collect.findOne({"_id":user_id}).size():
+    if db.collect.findOne({"_id":user_id}).size():
         abort(409, message="User already exists")
 
 class User(Resource):
