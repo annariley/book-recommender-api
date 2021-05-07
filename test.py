c4f1c9131f3e7b8663132216458ba5cc1b0d1e3d
annariley <annamriley4@gmail.com>
2021-05-07 08:29:03 -0700

    edit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "user/" + 1, {"_id":1,
+response = requests.put(BASE + "user/" + "1", {"_id":1,
 "book 1 id": 1234,"book 1 rating": 4,
 "book 2 id": 5678,"book 2 rating": 2, 
 "book 3 id": 9101,"book 3 rating": 5,
