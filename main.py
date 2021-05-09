9b323c1a00a4bfc89d1cbf4b8cf0103532d7a6c8
annariley <annamriley4@gmail.com>
2021-05-08 23:36:40 -0700

    edit

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class User(Resource):
         sentiment = args["sentiment"]
         swipe_data = db["swipe-data"]
         swipe_data.insert_one({str(user_id):(book_id, sentiment)})
-        return {book_id:sentiment}
+        return (book_id,sentiment)
 
 api.add_resource(User, "/user/<int:user_id>")
 
