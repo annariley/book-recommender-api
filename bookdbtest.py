20b0ca8eedc8c89f083f98d4565ae02ebe21a2a7
annariley <annamriley4@gmail.com>
2021-05-13 12:45:11 -0500

    dataset db testing

---
+++ bookdbtest.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+from flask import request
+from flask import jsonify
+from flask_pymongo import PyMongo
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'bookuser-db'
+app.config["MONGO_URI"] = "mongodb+srv://dbUser:cpen291@cluster0.02dfd.mongodb.net/book-recommender?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+books = pd.read_csv('goodbooks.csv') 
+books_book_conv = {m : m-1 for m in self.books['book_id'] }
+books['book_id'] = self.books['book_id'].apply(lambda m: books_book_conv[m])
+books_dict = self.books.to_dict('book_data')
+client = pymongo.MongoClient("mongodb+srv://dbUser:cpen291@cluster0.02dfd.mongodb.net/book-recommender?retryWrites=true&w=majority")
+db = client["book-recommender"]
+book_collect = db["book-data"]
+book_collect.insert_many(books_dict, ordered=False)
