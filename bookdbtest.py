012add39ce3939968d801261eaa6cb07ce912946
annariley <annamriley4@gmail.com>
2021-05-13 21:51:01 -0500

    edit

--- bookdbtest.py
+++ bookdbtest.py
@@ -13,10 +13,9 @@ mongo = PyMongo(app)
 books = pd.read_csv('books.csv') 
 books_book_conv = {m : m-1 for m in books['book_id'] }
 books['book_id'] = books['book_id'].apply(lambda m: books_book_conv[m])
-books.drop(columns=['goodreads_book_id', 'best_book_id', 'work_id', 'books_count', 'isbn','isbn13','original_publication_year','original_title','language_code','average_rating','ratings_count','work_ratings_count','work_text_reviews_count','ratings_1','ratings_2','ratings_3','ratings_4','ratings_5','small_image_url'])
+books.drop(['goodreads_book_id', 'best_book_id', 'work_id', 'books_count', 'isbn','isbn13','original_publication_year','original_title','language_code','average_rating','ratings_count','work_ratings_count','work_text_reviews_count','ratings_1','ratings_2','ratings_3','ratings_4','ratings_5','small_image_url'], axis=1)
 print(books)
 books_dict = books.to_dict('records')
-print(books_dict)
 client = pymongo.MongoClient("mongodb+srv://dbUser:cpen291@cluster0.02dfd.mongodb.net/book-recommender?retryWrites=true&w=majority")
 db = client["book-recommender"]
 book_collect = db["book-data"]
