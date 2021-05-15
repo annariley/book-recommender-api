ca0e1e066702f6104b1444b0d5bc549e6fecd4fb
annariley <annamriley4@gmail.com>
2021-05-15 06:50:02 -0500

    test

--- dsdbstore.py
+++ dsdbstore.py
@@ -15,7 +15,7 @@ mongo = PyMongo(app)
 client = pymongo.MongoClient("mongodb+srv://dbUser:cpen291@cluster0.02dfd.mongodb.net/book-recommender?retryWrites=true&w=majority")
 db = client["book-recommender"]
 book_collect = db["book-data"]
-print(book_collect.find_one({"book_id":1})["title"])
+print(book_collect.find_one({"book_id":1})["authors"])
 
 #Not sure if the below code should stay here or be elsewhere. We will need wherever we train our models. Not sure if we need it in the backnd anymore, now that book info
 #is in Mongo.
